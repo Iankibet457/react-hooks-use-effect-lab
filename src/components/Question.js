76ae7ef47c38daf04e389d51187cedab28a001ed
Ian <ian.mosonik@student.moringaschool.com>
2024-11-06 13:32:48 +0300

    completed lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,24 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      onAnswered(false)
+      setTimeRemaining(10); 
+    } else {
+      const timer = setTimeout(() => {
+        setTimeRemaining(timeRemaining - 1);  
+      }, 1000);
+
+      
+      return () => clearTimeout(timer);
+    }
+  }, [timeRemaining]);
+  
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
